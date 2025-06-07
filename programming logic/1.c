78944d2c418ac616934982cc2bfcbc288667ece5
gaeunee2 <163801908+gaeunee2@users.noreply.github.com>
2025-06-07 13:36:56 +0900

    Add files via upload

---
+++ programming logic/1.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int month, days;
+
+	scanf("%d", &month);
+
+	if (month < 1 || month>12) {
+		printf("invalid month\n");
+		return 0;
+	}
+	else {
+		switch (month) {
+		case 2:
+			days = 28;
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			days = 30;
+			break;
+		default:
+			days = 31;
+			break;
+		}
+	}
+	printf("%d\n", days);
+
+	return 0;
+}
