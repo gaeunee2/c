5dac095b1afbb99e2328aecaf0803090306ef5bd
gaeunee2 <163801908+gaeunee2@users.noreply.github.com>
2025-06-07 13:32:55 +0900

    Add files via upload

---
+++ programming logic/1.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int i, n, sum = 0;
+
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++)
+		if (n % i == 0)
+			sum += i;
+	printf("%d\n", sum);
+
+	return 0;
+}
